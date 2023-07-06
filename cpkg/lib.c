760f0cb6f48ffac2b34f84c6a12e6a83ba5b6771
MatusNatalia <nematus3@gmail.com>
2023-07-06 16:08:55 +0700

    cpkg

--- cpkg/lib.c
+++ cpkg/lib.c
@@ -1,9 +1,11 @@
 #include <fcntl.h>
 #include <dlfcn.h>
+#include <netinet/in.h>
 #include "lib.h"
 
 void* get_hDl(){
-    return dlopen("unistd.h", RTLD_LAZY);
+    char* lib_name = "unistd.h";
+    return dlopen(lib_name, RTLD_LAZY);
 }
 
 Write_callback get_real_write(){
@@ -29,21 +31,27 @@ Close_callback get_real_close(){
 
 int connect(int sock_fd, const struct sockaddr *addr, socklen_t addrlen){
     int tmp_sock_fd;
+    struct sockaddr_in* sin = (struct sockaddr_in*)addr;
 
     void *hDl = get_hDl();
 
     Write_callback real_write = get_real_write();
     Close_callback real_close = get_real_close();
 
+    bson_t b = BSON_INITIALIZER;
+    BSON_APPEND_UTF8(&b, "Call", "connect");
+    BSON_APPEND_INT32(&b, "SockFd", sock_fd);
+    BSON_APPEND_INT32(&b, "Port", sin->sin_port);
+    BSON_APPEND_INT32(&b, "Ip", sin->sin_addr.s_addr);
+
     tmp_sock_fd = open("/tmp/vpnchains.socket", O_RDWR);
-    real_write(tmp_sock_fd, &sock_fd, sizeof(int));
-    real_write(tmp_sock_fd, (void*)addr, addrlen);
+    real_write(tmp_sock_fd, &b, b.len);
     real_close(tmp_sock_fd);
 
     return 0;
 }
 
-size_t read(int sock_fd, void *buf, size_t count){
+ssize_t read(int sock_fd, void *buf, size_t count){
     int tmp_sock_fd;
     int n;
 
@@ -51,23 +59,33 @@ size_t read(int sock_fd, void *buf, size_t count){
     Write_callback real_write = get_real_write();
     Close_callback real_close = get_real_close();
 
+    bson_t b = BSON_INITIALIZER;
+    BSON_APPEND_UTF8(&b, "Call", "read");
+    BSON_APPEND_INT32(&b, "Fd", sock_fd);
+    BSON_APPEND_INT32(&b, "BytesToRead", count);
+
     tmp_sock_fd = open("/tmp/vpnchains.socket", O_RDWR);
-    real_write(tmp_sock_fd, &sock_fd, sizeof(int));
-    n = real_read(tmp_sock_fd, buf, count);
+    real_write(tmp_sock_fd, &b, b.len);
+    //n = real_read(tmp_sock_fd, buf, count);
     real_close(tmp_sock_fd);
 
     return n;
 }
 
-int write(int sock_fd, void *buf, size_t count){
+ssize_t write(int sock_fd, const void *buf, size_t count){
     int tmp_sock_fd;
 
     Write_callback real_write = get_real_write();
-    Close_callback real_close = get_real_close();
+    Close_callback real_close =get_real_close();
+
+    bson_t b = BSON_INITIALIZER;
+    BSON_APPEND_UTF8(&b, "Call", "write");
+    BSON_APPEND_INT32(&b, "Fd", sock_fd);
+    //BSON_APPEND_(&b, "Buffer", ??);
+    BSON_APPEND_INT32(&b, "BytesToWrite", count);
 
     tmp_sock_fd = open("/tmp/vpnchains.socket", O_RDWR);
-    real_write(tmp_sock_fd, &sock_fd, sizeof(int));
-    real_write(tmp_sock_fd, buf, count);
+    real_write(tmp_sock_fd, &b, b.len);
     real_close(tmp_sock_fd);
 
     return 0;
