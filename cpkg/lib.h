8c7bc6dbe0c055d89f09dd85654bbf900aeb03d8
MatusNatalia <nematus3@gmail.com>
2023-07-07 13:00:44 +0700

    real_connect

--- cpkg/lib.h
+++ cpkg/lib.h
@@ -1,10 +1,10 @@
 #pragma once
 #include <inttypes.h>
-#include <sys/socket.h>
 #include <libbson-1.0/bson/bson.h>
 
 typedef size_t (*Read_callback)(int, void*, size_t);
 typedef int (*Write_callback)(int, const void*, size_t);
+typedef int (*Connect_callback)(int, const struct sockaddr*, socklen_t);
 typedef int (*Close_callback)(int);
 
 void* get_hDl();
