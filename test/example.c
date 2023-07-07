07d64de0464f66517c7110fde54a3fb893d3e856
Pavel Smolyakov <pavel.smolyakov.83@gmail.com>
2023-07-07 14:42:26 +0700

    example.c (для тестов тупа)

---
+++ test/example.c
@@ -0,0 +1,35 @@
+#include <manifest.h>
+#include <bsdtypes.h>
+#include <in.h>
+#include <socket.h>
+#include <netdb.h>
+#include <stdio.h>
+
+
+int main() {
+    unsigned short port;       /* port client will connect to         */
+    char buf[12];              /* data buffer for sending & receiving */
+    struct hostent *hostnm;    /* server host name information        */
+    struct sockaddr_in server; /* server address                      */
+    int s;                     /* client socket                       */
+
+    hostnm = gethostbyname("lib.ru")
+    port = 80
+
+    server.sin_family      = AF_INET;
+    server.sin_port        = htons(port);
+    server.sin_addr.s_addr = *((unsigned long *)hostnm->h_addr);
+
+    if ((s = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
+        tcperror("Socket()");
+        exit(3);
+    }
+
+    if (connect(s, (struct sockaddr *)&server, sizeof(server)) < 0)
+    {
+        tcperror("Connect()");
+        exit(4);
+    }
+jn
+}
