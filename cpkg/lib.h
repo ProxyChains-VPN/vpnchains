7cc1832d198742fb3cc9b6f31c2f13acd620b2ab
Pavel Smolyakov <pavel.smolyakov.83@gmail.com>
2023-07-10 10:16:02 +0700

    убрал штуки из хедера чтобы пропали варнинги

--- cpkg/lib.h
+++ cpkg/lib.h
@@ -1,16 +1,3 @@
 #pragma once
-#include <inttypes.h>
-#include <libbson-1.0/bson/bson.h>
 
-typedef size_t (*Read_callback)(int, void*, size_t);
-typedef int (*Write_callback)(int, const void*, size_t);
-typedef int (*Connect_callback)(int, const struct sockaddr*, socklen_t);
-typedef int (*Close_callback)(int);
-
-void* get_hDl();
-
-//int connect(int, const struct sockaddr*, socklen_t);
-//ssize_t read(int, void*, size_t);
-//ssize_t write(int, const void*, size_t);
-
-#define SO_VISIBLE __attribute__((visibility("default")))
+#define SO_EXPORT __attribute__((visibility("default")))
