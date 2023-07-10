3ef9a871b20fc8a16fc4c79ed09eb322d51c86f5
Pavel Smolyakov <pavel.smolyakov.83@gmail.com>
2023-07-10 09:33:56 +0700

    привели в порядок буковки в бсоне

--- gopkg/ipc/ipc_messages.go
+++ gopkg/ipc/ipc_messages.go
@@ -1,30 +1,30 @@
 package ipc
 
 type ConnectRequest struct {
-	SockFd int32
-	Port   uint16
-	Ip     uint32
+	SockFd int32  `bson:"sock_fd"`
+	Port   uint16 `bson:"port"`
+	Ip     uint32 `bson:"ip"`
 }
 
 // TODO написать чтобы буковки сериализовывались с сохранением регистра
 type ConnectResponse struct {
-	ResultCode int32 // 0 или -1
+	ResultCode int32 `bson:"result_code"`
 }
 
 type ReadRequest struct {
-	Fd          int32
-	BytesToRead int32
+	Fd          int32 `bson:"fd"`
+	BytesToRead int32 `bson:"bytes_to_read"`
 }
 
 type ReadResponse struct {
-	Buffer    string
-	BytesRead int32
+	Buffer    string `bson:"buffer"`
+	BytesRead int32  `bson:"bytes_read"`
 }
 
 type WriteRequest struct {
-	Fd           int32
-	Buffer       []byte
-	BytesToWrite int32
+	Fd           int32  `bson:"fd"`
+	Buffer       []byte `bson:"buffer"`
+	BytesToWrite int32  `bson:"bytes_to_write"`
 }
 
 type WriteResponse struct {
