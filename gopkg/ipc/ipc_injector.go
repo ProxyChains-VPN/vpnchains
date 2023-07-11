23650f3f8ffc0d102e7b649acfebaf185dd6518b
Pavel Smolyakov <pavel.smolyakov.83@gmail.com>
2023-07-11 10:42:26 +0700

    немного документации

--- gopkg/ipc/ipc_injector.go
+++ gopkg/ipc/ipc_injector.go
@@ -5,6 +5,12 @@ import (
 	"os/exec"
 )
 
+// CreateCommandWithInjectedLibrary creates a new exec.Cmd instance with an injected library.
+// In other words, it inserts a library whose path is libPath into the command's LD_PRELOAD environment variable,
+// and returns the command.
+// libPath - path to the library
+// commandPath - path to the command
+// args - command arguments
 func CreateCommandWithInjectedLibrary(libPath string, commandPath string, args []string) *exec.Cmd {
 	cmd := exec.Command(commandPath, args...)
 
