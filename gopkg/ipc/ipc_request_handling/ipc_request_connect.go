1fddbb9bbe758278329d4d00da0ecdbebbe60f99
MatusNatalia <nematus3@gmail.com>
2023-07-11 17:09:10 +0700

    update processConnect

--- gopkg/ipc/ipc_request_handling/ipc_request_connect.go
+++ gopkg/ipc/ipc_request_handling/ipc_request_connect.go
@@ -3,6 +3,8 @@ package ipc_request_handling
 import (
 	"go.mongodb.org/mongo-driver/bson"
 	"vpnchains/gopkg/ipc"
+	"vpnchains/gopkg/vpn/wireguard"
+	"syscall"
 )
 
 var errorConnectResponse = ipc.ConnectResponse{ResultCode: -1}
@@ -10,5 +12,14 @@ var errorConnectResponseBytes, _ = bson.Marshal(errorConnectResponse)
 
 func (handler *RequestHandler) processConnect(request *ipc.ConnectRequest) (*ipc.ConnectResponse, error) {
 	//log.Println(request)
+
+	/*var sa syscall.SockaddrInet4
+	sa.Addr[0] = byte(request.Ip >> 24)
+	sa.Addr[1] = byte(request.Ip >> 16)
+	sa.Addr[2] = byte(request.Ip >> 8)
+	sa.Addr[3] = byte(request.Ip)
+	sa.Port = int(request.Port)
+	connect4(request.SockFd, &sa)*/
+
 	return &ipc.ConnectResponse{0}, nil
 }
