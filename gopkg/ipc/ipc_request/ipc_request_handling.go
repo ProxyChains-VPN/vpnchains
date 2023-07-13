77eb94251247195008ed48f3619e8211582ca6f0
Pavel Smolyakov <pavel.smolyakov.83@gmail.com>
2023-07-13 17:23:39 +0700

    я все поломал оно теперь не скомпилируется

---
+++ gopkg/ipc/ipc_request/ipc_request_handling.go
@@ -0,0 +1,32 @@
+package ipc_request
+
+import (
+	"errors"
+	"go.mongodb.org/mongo-driver/bson"
+	"log"
+	"vpnchains/gopkg/vpn"
+)
+
+type RequestHandler struct {
+	tunnel vpn.Tunnel
+}
+
+func NewRequestHandler(tunnel vpn.Tunnel) *RequestHandler {
+	return &RequestHandler{tunnel: tunnel}
+}
+
+func (handler *RequestHandler) ParseRequestType(requestBytes []byte) (string, error) {
+	err := bson.Raw(requestBytes).Validate()
+	if err != nil {
+		return "", err
+	}
+
+	callValue := bson.Raw(requestBytes).Lookup("call")
+	call, ok := callValue.StringValueOK()
+	if !ok {
+		log.Println(callValue.Value)
+		return "", errors.New("call is not string")
+	} else {
+		return call, nil
+	}
+}
