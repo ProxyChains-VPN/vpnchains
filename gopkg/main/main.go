c82c5c303d6d456eaaf1e80ed0cbe53e1ebfd340
Pavel Smolyakov <pavel.smolyakov.83@gmail.com>
2023-07-20 11:04:39 +0500

    рефакторинг в процессе

--- gopkg/main/main.go
+++ gopkg/main/main.go
@@ -8,14 +8,37 @@ import (
 	"vpnchains/gopkg/vpn/wireguard"
 )
 
-const DefaultSockAddr = "/tmp/vpnchains.socket"
-const InjectedLibPath = "/usr/lib/libvpnchains_inject.so"
-const BufSize = 100500
-const Mtu = 1420
+// DefaultIpcServerPort is the default port for the IPC server.
+// If the port is neither specified in flags nor in the environment (VPNCHAINS_IPC_SERVER_PORT),
+// this port (45454, I guess) will be used.
+const DefaultIpcServerPort = 45454
+
+// DefaultInjectedLibPath is the default path to the injected library.
+// If the path is not specified in the environment (VPNCHAINS_INJECT_LIB_PATH),
+// this path (/usr/lib/libvpnchains_inject.so, I guess) will be used.
+const DefaultInjectedLibPath = "/usr/lib/libvpnchains_inject.so"
+
+// DefaultBufSize is the default size of the buffer used for reading from the socket.
+// If the size is neither specified in flags nor in the environment (VPNCHAINS_BUF_SIZE),
+// this size (100500, I guess) will be used.
+const DefaultBufSize = 100500
+
+// DefaultMtu is the default mtu for the wireguard tunnel.
+// If the mtu is neither specified in flags nor in the environment (VPNCHAINS_MTU),
+// this amount (1420, I guess) will be used.
+const DefaultMtu = 1420
 
 func errorMsg(path string) string {
 	return "Usage: " + path + " <config> " +
 		"<command> [command args...]"
+}
+
+func getIpcServerPort() int {
+
+}
+
+func environParseServerPort() {
+
 }
 
 func main() {
